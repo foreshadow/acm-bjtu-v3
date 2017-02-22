8e77688ac6cd786900b24d454c5c107fdcd5daf7
Infinity <573679956@qq.com>
2017-02-22 22:42:54 +0800

    Update contest panel

--- app/InfoContest.php
+++ app/InfoContest.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class InfoContest extends Model
 {
-    //
+    public static function filter()
+    {
+        $infos = InfoContest::orderBy('start_time')
+                            ->where('start_time', '>=', date('Y-m-d H:i:s', time() - 7200))
+                            ->whereIn('oj', ['Topcoder', 'Codeforces', 'BestCoder', 'Leetcode', 'AtCoder'])
+                            ->take(4)->get();
+        return $infos;
+    }
 }

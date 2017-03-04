73c917b9f587e9ebcbb24319265fe4489c591fd0
Infinity <573679956@qq.com>
2017-03-04 17:48:14 +0800

    Update contest style

--- resources/views/contest.blade.php
+++ resources/views/contest.blade.php
@@ -9,36 +9,38 @@
   $contests = $infos;
   @endphp
   <div class="container">
-    <h3>所有比赛</h3>
-    <table class="table table-striped table-hover">
-      <thead>
-        <tr>
-          <th>网站</th>
-          <th>比赛</th>
-          <th>时间</th>
-          <th></th>
-          <th>链接</th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach($contests as $contest)
+    <div class="col-md-8 col-md-offset-2">
+      <h3>所有比赛</h3>
+      <table class="table table-striped table-hover">
+        <thead>
           <tr>
-            <td>{{ $contest->oj }}</td>
-            <td>
-              <a href="{{ $contest->link }}">{{ $contest['name'] }}</a>
-            </td>
-            <td class="@if ($info['startTimeSeconds'] < time()) text-danger @endif">
-              <small>{{ date('l, F jS H:i', $contest['startTimeSeconds']) }}</small>
-            </td>
-            <td>
-              <small>{{ \App\Utilities\Functions::relative_time($contest['startTimeSeconds'] - time()) }}</small>
-            </td>
-            <td>
-              <small><a href="//{{ parse_url($contest->link)['host'] }}">{{ parse_url($contest->link)['host'] }}</a></small>
-            </td>
+            <th>网站</th>
+            <th>比赛</th>
+            <th>时间</th>
+            <th></th>
+            <th class="hidden">链接</th>
           </tr>
-        @endforeach
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          @foreach($contests as $contest)
+            <tr>
+              <td>{{ $contest->oj }}</td>
+              <td>
+                <a href="{{ $contest->link }}">{{ $contest['name'] }}</a>
+              </td>
+              <td class="@if ($info['startTimeSeconds'] < time()) text-danger @endif">
+                <small>{{ date('l, F jS H:i', $contest['startTimeSeconds']) }}</small>
+              </td>
+              <td>
+                <small>{{ \App\Utilities\Functions::relative_time($contest['startTimeSeconds'] - time()) }}</small>
+              </td>
+              <td class="hidden">
+                <small><a href="//{{ parse_url($contest->link)['host'] }}">{{ parse_url($contest->link)['host'] }}</a></small>
+              </td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
   </div>
 @endsection

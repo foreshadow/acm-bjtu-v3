dd16eab75889d5b24c98bdf250149a0577fb3769
Infinity <573679956@qq.com>
2017-03-04 17:34:29 +0800

    Add codeforces into info

--- crawler/acmicpc.py
+++ crawler/acmicpc.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     html = urllib.urlopen(url).read()
     j = json.loads(html)
     for contest in j:
-        if contest['oj'] in ['BestCoder', 'Topcoder']:
+        if contest['oj'] in ['BestCoder', 'Topcoder', 'Codeforces']:
             contest.pop('week')
             contest.pop('access')
             if contest['name'][:3] == 'SRM':

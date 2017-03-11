25bb663ad1e23b9129790a0e4e93291e686ff08b
Infinity <573679956@qq.com>
2017-03-11 19:14:14 +0800

    Create markdown.js

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ require('laravel-elixir-vue-2');
 elixir((mix) => {
     mix.sass('app.scss')
        .webpack('app.js');
-    mix.copy('node_modules/bootstrap-sass/assets/fonts/bootstrap/','public/fonts/bootstrap');
     mix.sass('print.scss');
+    mix.copy('resources/assets/js/markdown.min.js','public/js');
+    mix.copy('node_modules/bootstrap-sass/assets/fonts/bootstrap/','public/fonts/bootstrap');
 });

08b95634e02cdfd808ed67dc1af10aa0c9bce679
Infinity <573679956@qq.com>
2017-03-27 14:55:52 +0800

    Update raw.blade.php

--- resources/views/raw.blade.php
+++ resources/views/raw.blade.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/styles/github-gist.min.css">
 
-<pre><code style="background: white; font-family: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace; font-size: 12px;">{{ $content }}</code></pre>
+<pre><code style="background: white; font-family: &quot;SFMono-Regular&quot;, Consolas, &quot;Liberation Mono&quot;, Menlo, Courier, monospace; font-size: 12px;">{{ $content }}</code></pre>
 
 <!-- Markdown code highlighter -->
 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/highlight.min.js"></script>

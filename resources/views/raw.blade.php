8940a41ca15c0f583d329f2a22a3a670d048f25c
Infinity <573679956@qq.com>
2017-03-27 14:48:04 +0800

    Update raw.blade.php

--- resources/views/raw.blade.php
+++ resources/views/raw.blade.php
@@ -1,6 +1,6 @@
 <pre><code style="background: white;">{{ $content }}</code></pre>
 
 <!-- Markdown code highlighter -->
-<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/styles/github.min.css">
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/styles/github-gist.min.css">
 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/highlight.min.js"></script>
 <script>hljs.initHighlightingOnLoad();</script>

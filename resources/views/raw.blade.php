eb33f14b3d4a790348fa6309a264a229c4e2c7f0
Infinity <573679956@qq.com>
2017-03-27 14:52:53 +0800

    Update raw.blade.php

--- resources/views/raw.blade.php
+++ resources/views/raw.blade.php
@@ -1,6 +1,7 @@
-<pre><code style="background: white;">{{ $content }}</code></pre>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/styles/github-gist.min.css">
+
+<pre><code style="background: white; font-family: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace; font-size: 12px;">{{ $content }}</code></pre>
 
 <!-- Markdown code highlighter -->
-<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/styles/github-gist.min.css">
 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/highlight.min.js"></script>
 <script>hljs.initHighlightingOnLoad();</script>

e9c3d7cae8031b19c6cd9f3b83fc1d4321c04c2a
Infinity <573679956@qq.com>
2017-04-06 12:53:38 +0800

    Fix bug: handle exception

--- crawler/info.py
+++ crawler/info.py
@@ -8,9 +8,12 @@ if __name__ == '__main__':
     db = mysqlbuilder.database(debug = True)
     db.query('select handle from users', [])
     for row in db.cursor.fetchall():
-        handle = row[0]
-        if handle:
-            html = urllib.urlopen(url.format(handle)).read()
-            j = json.loads(html)
-            if j['status'] == 'OK':
-                db.insert_or_update(j['result'][0], 'codeforces_users', 'handle')
+        try:
+            handle = row[0]
+            if handle:
+                html = urllib.urlopen(url.format(handle)).read()
+                j = json.loads(html)
+                if j['status'] == 'OK':
+                    db.insert_or_update(j['result'][0], 'codeforces_users', 'handle')
+        except Exception, e:
+            print e
